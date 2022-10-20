5ead640c995cb191f97f62bda1f6f1001874a970
VikashK21 <vikash21@navgurukul.org>
2022-10-20 14:54:06 +0530

    the backend too

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 var io = require("socket.io")({
   path: "/io/webrtc",
@@ -7,13 +8,23 @@ var io = require("socket.io")({
 const app = express();
 const port = 8080;
 
+const rooms = {};
 // app.get('/', (req, res) => res.send('Hello World!!!!!'))
 
 //https://expressjs.com/en/guide/writing-middleware.html
-app.use(express.static("../client/build"));
-// app.get('/', (req, res, next) => {
-//     res.sendFile(__dirname + '/build/index.html')
-// })
+// console.log(path.join(__dirname, "../client/build/index.html"));
+app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/public")));
+// app.use(express.static())
+app.get("/", (req, res, next) => {
+  //default room
+  // /home/vikash/Desktop/BASK/bb_video_call/client/build/index.html
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.get("/:room", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const server = app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`),
@@ -30,37 +41,63 @@ io.on("connection", (socket) => {
 const peers = io.of("/webrtcPeer");
 
 // keep a reference of all socket connections
-let connectedPeers = new Map();
+// let connectedPeers = new Map();
 
 peers.on("connection", (socket) => {
-  connectedPeers.set(socket.id, socket);
+  const room = socket.handshake.query.room;
+
+  rooms[room] =
+    (rooms[room] && rooms[room].set(socket.id, socket)) ||
+    new Map().set(socket.id, socket);
+
+  // connectedPeers.set(socket.id, socket);
 
   console.log(socket.id);
   socket.emit("connection-success", {
     success: socket.id,
-    peerCount: connectedPeers.size,
+    peerCount: rooms[room].size,
   });
 
-  const broadcast = () =>
-    socket.broadcast.emit("joined-peers", {
-      peerCount: connectedPeers.size,
-    });
+  // const broadcast = () => socket.broadcast.emit('joined-peers', {
+  //   peerCount: connectedPeers.size,
+  // })
+  const broadcast = () => {
+    const _connectedPeers = rooms[room];
+
+    for (const [socketID, _socket] of _connectedPeers.entries()) {
+      // if (socketID !== socket.id) {
+      _socket.emit("joined-peers", {
+        peerCount: rooms[room].size, //connectedPeers.size,
+      });
+      // }
+    }
+  };
   broadcast();
 
-  const disconnectedPeer = (socketID) =>
-    socket.broadcast.emit("peer-disconnected", {
-      peerCount: connectedPeers.size,
-      socketID: socketID,
-    });
+  // const disconnectedPeer = (socketID) => socket.broadcast.emit('peer-disconnected', {
+  //   peerCount: connectedPeers.size,
+  //   socketID: socketID
+  // })
+  const disconnectedPeer = (socketID) => {
+    const _connectedPeers = rooms[room];
+    for (const [_socketID, _socket] of _connectedPeers.entries()) {
+      _socket.emit("peer-disconnected", {
+        peerCount: rooms[room].size,
+        socketID,
+      });
+    }
+  };
 
   socket.on("disconnect", () => {
     console.log("disconnected");
-    connectedPeers.delete(socket.id);
+    // connectedPeers.delete(socket.id)
+    rooms[room].delete(socket.id);
     disconnectedPeer(socket.id);
   });
 
   socket.on("onlinePeers", (data) => {
-    for (const [socketID, _socket] of connectedPeers.entries()) {
+    const _connectedPeers = rooms[room];
+    for (const [socketID, _socket] of _connectedPeers.entries()) {
       // don't send to self
       if (socketID !== data.socketID.local) {
         console.log("online-peer", data.socketID, socketID);
@@ -70,7 +107,8 @@ peers.on("connection", (socket) => {
   });
 
   socket.on("offer", (data) => {
-    for (const [socketID, socket] of connectedPeers.entries()) {
+    const _connectedPeers = rooms[room];
+    for (const [socketID, socket] of _connectedPeers.entries()) {
       // don't send to self
       if (socketID === data.socketID.remote) {
         // console.log('Offer', socketID, data.socketID, data.payload.type)
@@ -83,7 +121,8 @@ peers.on("connection", (socket) => {
   });
 
   socket.on("answer", (data) => {
-    for (const [socketID, socket] of connectedPeers.entries()) {
+    const _connectedPeers = rooms[room];
+    for (let [socketID, socket] of _connectedPeers.entries()) {
       if (socketID === data.socketID.remote) {
         console.log("Answer", socketID, data.socketID, data.payload.type);
         socket.emit("answer", {
@@ -106,8 +145,9 @@ peers.on("connection", (socket) => {
   // })
 
   socket.on("candidate", (data) => {
+    const _connectedPeers = rooms[room];
     // send candidate to the other peer(s) if any
-    for (const [socketID, socket] of connectedPeers.entries()) {
+    for (let [socketID, socket] of _connectedPeers.entries()) {
       if (socketID === data.socketID.remote) {
         socket.emit("candidate", {
           candidate: data.payload,
