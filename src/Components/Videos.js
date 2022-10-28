116606dcc71616fe72844e7215ad72632397c841
VikashK21 <vikash21@navgurukul.org>
2022-10-28 11:06:28 +0530

    the messges and the mesh tepology video streaming

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,47 +1,60 @@
-import React, {Component} from 'react'
-import Video from './Video'
+import React, { Component } from "react";
+import Video from "./Video";
 
 class Videos extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       rVideos: [],
-      remoteStreams: []
-    }
+      remoteStreams: [],
+    };
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.remoteStreams !== nextProps.remoteStreams) {
-      
       let _rVideos = nextProps.remoteStreams.map((rVideo, index) => {
-        let video = <Video
-          videoStream={rVideo.stream}
-          frameStyle={{ width: 120, float: 'left', padding: '0 3px' }}
-          videoStyles={{
-            cursor: 'pointer',
-            objectFit: 'cover',
-            borderRadius: 3,
-            width: '100%',
-          }}
-        />
+        const _videoTrack = rVideo.stream
+          .getTracks()
+          .filter((track) => track.kind === "video");
+        console.log(
+          this.props.remoteStreams.length,
+          "the length of the videos",
+        );
+        let video = (_videoTrack && (
+          <Video
+            videoStream={rVideo.stream}
+            frameStyle={{ width: 120, float: "left", padding: "0 3px" }}
+            videoStyles={{
+              cursor: "pointer",
+              objectFit: "cover",
+              borderRadius: 3,
+              width: "100%",
+            }}
+            autoplay
+          />
+        )) || (
+          <div style={{ display: "inline-block" }}>
+            {this.props.switchVideo(nextProps.remoteStreams[0])}
+          </div>
+        );
 
         return (
           <div
             id={rVideo.name}
             onClick={() => this.props.switchVideo(rVideo)}
-            style={{ display: 'inline-block' }}
+            style={{ display: "inline-block" }}
             key={index}
           >
             {video}
           </div>
-        )
-      })
+        );
+      });
 
       this.setState({
         remoteStreams: nextProps.remoteStreams,
-        rVideos: _rVideos
-      })
+        rVideos: _rVideos,
+      });
     }
   }
 
@@ -50,23 +63,22 @@ class Videos extends Component {
       <div
         style={{
           zIndex: 3,
-          position: 'fixed',
-          padding: '6px 3px',
-          backgroundColor: 'rgba(0,0,0,0.3)',
+          position: "fixed",
+          padding: "6px 3px",
+          backgroundColor: "rgba(0,0,0,0.3)",
           maxHeight: 120,
-          top: 'auto',
+          top: "auto",
           right: 10,
           left: 10,
           bottom: 10,
-          overflowX: 'scroll',
-          whiteSpace: 'nowrap'
+          overflowX: "scroll",
+          whiteSpace: "nowrap",
         }}
       >
-        { this.state.rVideos }
+        {this.state.rVideos}
       </div>
-    )
+    );
   }
-
 }
 
-export default Videos
+export default Videos;
